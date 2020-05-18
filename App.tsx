d5e2e6c274620b03e1f8318081afdb1106518606
Jisqo <jayshooo1@gmail.com>
2020-05-18 21:43:53 +0900

    불필요한 Fragment 삭제

--- App.tsx
+++ App.tsx
@@ -8,12 +8,10 @@ declare const global: { HermesInternal: null | {}; };
 const App = () => {
 
     return (
-        <>
-            <View
-                style={ { flex: 1 } }>
-                <AppNavigation />
-            </View>
-        </>
+        <View
+            style={ { flex: 1 } }>
+            <AppNavigation />
+        </View>
     );
 
 };
