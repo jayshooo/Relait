cb44765649342901ed851037b432cb596cae6896
Jisqo <jayshooo1@gmail.com>
2020-06-30 15:13:50 +0900

    로그인 API 호출 타입정의 및 코드 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { BASE_URL } from './constants';
 
-export const httpClient = axios.create({
-    baseURL: BASE_URL,
-});
+export const httpClient = () => {
+
+    return axios.create({
+        baseURL: BASE_URL,
+    });
+
+};
--- src/store/saga/types.ts
+++ src/store/saga/types.ts
@@ -2,9 +2,7 @@ export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 
-interface LoginSagaAction {
+export interface LoginSagaAction {
     type: typeof LOGIN_REQUEST | typeof LOGIN_SUCCESS | typeof LOGIN_FAILED;
     data: string;
 }
-
-export type SagaActionTypes = LoginSagaAction;
