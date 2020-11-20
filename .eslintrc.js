2cd4698985b8a360e7c1b4bdf09e9bf442381a84
jayshooo <jayshooo1@gmail.com>
2020-11-20 12:11:58 +0900

    eslint 셋팅 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   "rules": {
-    "prettier/prettier": "off"
-  }
+    "prettier/prettier": "off",
+    "react-native/no-inline-styles": "off",
+    "quotes": [
+      "error",
+      "double",
+    ],
+    "no-console": "off",
+    "no-mixed-spaces-and-tabs": "error",
+    "react/jsx-curly-spacing": ["error", "always", { allowMultiline: true }],
+    "react/jsx-max-props-per-line": [1, { "maximum": 1 }],
+    "react/jsx-indent-props": [1, 'tab'],
+    "object-curly-spacing": ["error", "always"],
+    "computed-property-spacing": ["error", "always"],
+    "react/jsx-first-prop-new-line": [1, "always"],
+  },
 };
